7734b0325a64b081c38ba6c080fb7d96fb3e6351
SupraMensch <cwadeburt@gmail.com>
2018-10-10 21:33:54 -0500

    Instructions(full) and name

--- src/project_3/Project_3.java
+++ src/project_3/Project_3.java
@@ -8,6 +8,35 @@
 //
 //  Complete Programming Challenge 16: "2D Array Operations" on page 493 in the textbook.
 
+// Write a program that creates a two-dimensional array intialized with test daa. 
+//  Use any primtive data type that you wish.  The program should have the following methods.
+
+//  getTotal.  This method should accept a two-dimensional array as its argument and return the total of
+// all the values in the array.  
+
+// getAverage.  This method should accept a two-dimensional array as its argument and return the average 
+//  of all the values in the array.
+
+//  getRowTotal.  This mehtod should accept a two-dimensional array as its first argument and an integer as its second 
+// argument.  The second argument should be the subscript of a row in the array.  The method should return
+//  the totla of the values in the specified row.
+
+//  getColumnTotal.   This method should accept a two-dimensional array as its frist argument and an integer as its second 
+//  argument.  The second argument should be the subscript of a column in the array.  The method should 
+//  return the total of ht evalues in the specified column.
+
+//  getHighestInRow.  This method should accept a two-dimensional array as its first argument and an integer
+//  as its second argument.  The second argument should be a subscript of a row in the array.  The method
+//  should return the highest value in the specified row of the array.
+
+//  getLowestInRow.  This mehtod should accept a two-dimensional array as its first argument and an integer
+//  as its second second argument.  the second argument should be a subscript of a row in the array.  The method
+//  should return the lowest value in the specified row of the array.
+
+//  Demonstrate the methods somehow.
+
+
+
 package project_3;
 
 public class Project_3 {
