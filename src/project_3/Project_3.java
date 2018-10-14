53099d07e8d0dd6c09b8858053716500b14e90a5
SupraMensch <cwadeburt@gmail.com>
2018-10-13 20:57:41 -0500

    draft 3 getTotal Method added

--- src/project_3/Project_3.java
+++ src/project_3/Project_3.java
@@ -4,46 +4,34 @@
 
 //
 
-//  2D Array Operations (20 points)
-//
-//  Complete Programming Challenge 16: "2D Array Operations" on page 493 in the textbook.
-
-// Write a program that creates a two-dimensional array intialized with test daa. 
-//  Use any primtive data type that you wish.  The program should have the following methods.
-
-//  getTotal.  This method should accept a two-dimensional array as its argument and return the total of
-// all the values in the array.  
-
-// getAverage.  This method should accept a two-dimensional array as its argument and return the average 
-//  of all the values in the array.
-
-//  getRowTotal.  This mehtod should accept a two-dimensional array as its first argument and an integer as its second 
-// argument.  The second argument should be the subscript of a row in the array.  The method should return
-//  the totla of the values in the specified row.
-
-//  getColumnTotal.   This method should accept a two-dimensional array as its frist argument and an integer as its second 
-//  argument.  The second argument should be the subscript of a column in the array.  The method should 
-//  return the total of ht evalues in the specified column.
-
-//  getHighestInRow.  This method should accept a two-dimensional array as its first argument and an integer
-//  as its second argument.  The second argument should be a subscript of a row in the array.  The method
-//  should return the highest value in the specified row of the array.
-
-//  getLowestInRow.  This mehtod should accept a two-dimensional array as its first argument and an integer
-//  as its second second argument.  the second argument should be a subscript of a row in the array.  The method
-//  should return the lowest value in the specified row of the array.
-
-//  Demonstrate the methods somehow.
 
 
 
 package project_3;
 
-public class Project_3 {
+public class Project_3 
+{
 
     public static void main(String[] args) 
     {
-        // TODO code application logic here
+       int [][] numbers ={ {20, 40, 60 },{ 80, 90, 100},{120, 140, 160} };
+       getTotal(numbers);
+    }
+   
+
+    public static void getTotal(int[][] numbers)
+    {
+        int total = 0;
+        
+        for (int row = 0; row< numbers.length; row++)
+        {
+            for(int col = 0; col <numbers[row].length; col++)
+                total += numbers[row][col];
+        }
+        
+        System.out.println("The total of all of the values inthe array is: " + total);
     }
     
 }
+
+
